ebffc06010dc1a350e3a07b555167eca65d17e22
HappyRangani <happyrangani61@gmail.com>
2024-06-20 12:30:55 +0530

    Add files via upload

---
+++ task_2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	int x,y,result;
+	
+	printf("enter your value x =");
+	scanf("%d",&x);
+	
+	printf("enter your value y =");
+	scanf("%d",&y);
+	
+	result =x*x-2*x*y+y*y;
+	printf("Result =%d",result);
+}
